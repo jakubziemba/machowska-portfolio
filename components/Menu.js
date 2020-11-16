2b0419a0c0a782e4d4badcd1e718866d757df4e5
Jakub Ziemba <jayziembka@gmail.com>
2020-11-16 15:38:32 +0100

    Emphasize name in About section

--- components/Menu.js
+++ components/Menu.js
@@ -6,28 +6,28 @@ export default function Menu() {
   const [worksActive, setWorksActive] = useState(false);
   const [exActive, setExActive] = useState(false);
 
-  console.log(exActive);
   return (
     <div className={styles.menu}>
       <ul>
         <li onClick={() => setAboutActive(!aboutActive)}>About</li>
         <div className={aboutActive && styles.active}>
           <p className={styles.text}>
-            Roksana Machowska, was born in 1996 in Warsaw. She holds a
-            Master of Fine Arts. She obtained eight scholarships for
-            the best students for the artistic achievements: from the
-            Rector of the Maria Grzegorzewska University in Warsaw
-            (2016–2020), from the Capital City of Warsaw (2016–2019),
-            and the Minister of Science and Higher Education (2020).
-            She works in a variety of mediums: printmaking, digital
-            graphics, painting, artistic fabric, design and
-            installation. Her work for the bachelor’s thesis in Fine
-            Arts was a series of graphics “Tamed element”. She took
-            part in some group shows and held her solo exhibitions as
-            well; including one at the Władysław Hasior Gallery in
-            Zakopane (branch of the Tatra Museum), or at the Capital
-            Centre for Cultural Education in Warsaw. In her works she
-            is inspired by nature and abstraction forms.
+            <span>Roksana Machowska</span>, was born in 1996 in
+            Warsaw. She holds a Master of Fine Arts. She obtained
+            eight scholarships for the best students for the artistic
+            achievements: from the Rector of the Maria Grzegorzewska
+            University in Warsaw (2016–2020), from the Capital City of
+            Warsaw (2016–2019), and the Minister of Science and Higher
+            Education (2020). She works in a variety of mediums:
+            printmaking, digital graphics, painting, artistic fabric,
+            design and installation. Her work for the bachelor’s
+            thesis in Fine Arts was a series of graphics “Tamed
+            element”. She took part in some group shows and held her
+            solo exhibitions as well; including one at the Władysław
+            Hasior Gallery in Zakopane (branch of the Tatra Museum),
+            or at the Capital Centre for Cultural Education in Warsaw.
+            In her works she is inspired by nature and abstraction
+            forms.
           </p>
         </div>
         <li onClick={() => setWorksActive(!worksActive)}>Works</li>
