19732520b5760724a0eec098bebb843368fb6bbd
Jakub Ziemba <jayziembka@gmail.com>
2020-11-17 18:00:52 +0100

    Move code to separate components

---
+++ components/MenuItems/Works.js
@@ -0,0 +1,11 @@
+import styles from './Submenu.module.scss';
+
+export default function Works() {
+  return (
+    <div className={styles.works}>
+      <li>Paintings</li>
+      <li>Graphic Art</li>
+      <li>Posters</li>
+    </div>
+  );
+}
