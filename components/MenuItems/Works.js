3a1e47a03ae7aaab0366947a2bd993efa74387fb
Jakub Ziemba <jayziembka@gmail.com>
2020-11-17 22:36:14 +0100

    Add link to paintings page

--- components/MenuItems/Works.js
+++ components/MenuItems/Works.js
@@ -1,9 +1,12 @@
+import Link from 'next/link';
 import styles from './Submenu.module.scss';
 
 export default function Works() {
   return (
     <div className={styles.works}>
-      <li>Paintings</li>
+      <li>
+        <Link href='/works/paintings'>Paintings</Link>
+      </li>
       <li>Graphic Art</li>
       <li>Posters</li>
     </div>
