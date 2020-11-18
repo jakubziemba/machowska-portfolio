80faf6ec502fd97ecf9621e34adbeb0f0b48cf5a
Jakub Ziemba <jayziembka@gmail.com>
2020-11-18 19:53:00 +0100

    Delete Header component, add content

--- pages/works/paintings.js
+++ pages/works/paintings.js
@@ -1,13 +1,22 @@
-import Header from '../../components/Header';
-import styles from './Paintings.module.scss';
+import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import styles from './paintings.module.scss';
 
 export default function Paintings() {
   return (
-    <>
-      <Header />
-      <div className={styles.container}>
+    <div className={styles.container}>
+      <FontAwesomeIcon icon={faArrowLeft} />
+      <div className={styles.header}>
         <h1>Paintings</h1>
       </div>
-    </>
+      <div className={styles.content}>
+        <h2 className={styles.title}>Shape I / 2020</h2>
+        <p className={styles.materials}>
+          Acrylic / Printing Ink on canvas.
+        </p>
+        <p className={styles.size}>100 x 70 cm</p>
+      </div>
+    </div>
   );
 }
