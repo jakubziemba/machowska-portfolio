768949db5a24a6010c75aaff62a73e4249d5db95
Jakub Ziemba <jayziembka@gmail.com>
2020-11-18 19:51:31 +0100

    Delete unnecessary menu button

--- components/Header.js
+++ components/Header.js
@@ -4,8 +4,6 @@ import styles from './Header.module.scss';
 export default function Header() {
   return (
     <header className={styles.header}>
-      <p className={styles.menu}>Menu</p>
-
       <Link href='/'>
         <a className={styles.home}>
           Roksana <br /> Machowska
