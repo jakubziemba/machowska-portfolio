7c4cb8f64aa9fb145602283a54806367f87622a8
Jakub Ziemba <jayziembka@gmail.com>
2020-11-19 23:22:28 +0100

    Create Work component

---
+++ pages/works/work.js
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+import styles from './work.module.scss';
+import { paintings } from './resources';
+
+export default function Work() {
+  return (
+    <div className={styles.content}>
+      {paintings.map((painting) => {
+        return (
+          <div className={styles.container} key={painting.id}>
+            <h2 className={styles.title}>{painting.title}</h2>
+            <p className={styles.materials}>{painting.materials}</p>
+            <p className={styles.size}>{painting.size}</p>
+            {painting.path.map((path) => (
+              <div className={styles.imageWrapper}>
+                <Image
+                  src={path}
+                  alt='Picture of the "Shape 1" painting'
+                  width={593}
+                  height={892}
+                />
+              </div>
+            ))}
+            {/* <div className={styles.imageWrapper}>
+                <Image
+                  src='/images/paintings/shape-1-2020/2.png'
+                  alt='Picture of the "Shape 1" painting'
+                  width={593}
+                  height={892}
+                />
+              </div> */}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
