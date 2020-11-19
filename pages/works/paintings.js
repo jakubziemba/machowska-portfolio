5e68646c4dce32db5136cc797cc50b0f51616dc5
Jakub Ziemba <jayziembka@gmail.com>
2020-11-19 23:19:38 +0100

    Move code to Work component

--- pages/works/paintings.js
+++ pages/works/paintings.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import Work from './work';
 import styles from './paintings.module.scss';
 
 export default function Paintings() {
@@ -10,13 +11,7 @@ export default function Paintings() {
       <div className={styles.header}>
         <h1>Paintings</h1>
       </div>
-      <div className={styles.content}>
-        <h2 className={styles.title}>Shape I / 2020</h2>
-        <p className={styles.materials}>
-          Acrylic / Printing Ink on canvas.
-        </p>
-        <p className={styles.size}>100 x 70 cm</p>
-      </div>
+      <Work />
     </div>
   );
 }
