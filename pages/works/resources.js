2af0c2226da1ed12b2e5d5266f534067e67d43a6
Jakub Ziemba <jayziembka@gmail.com>
2020-11-19 23:22:08 +0100

    Create works resources file

---
+++ pages/works/resources.js
@@ -0,0 +1,24 @@
+const paintings = [
+  {
+    id: 1,
+    title: 'Shape I / 2020',
+    materials: 'Acrylic / Printing Ink on canvas.',
+    size: '100 x 70 cm',
+    path: [
+      '/images/paintings/shape-1-2020/1.png',
+      '/images/paintings/shape-1-2020/2.png',
+    ],
+  },
+  {
+    id: 2,
+    title: 'Abstraction / 2019',
+    materials: 'Acrylic / Printing Ink on paperboard.',
+    size: '50 x 70 cm',
+    path: [
+      '/images/paintings/abstraction-2019/1.png',
+      '/images/paintings/abstraction-2019/2.png',
+    ],
+  },
+];
+
+export { paintings };
