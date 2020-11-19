8d1c28e6f7ded81d240cacbee8c123c28fcac967
Jakub Ziemba <jayziembka@gmail.com>
2020-11-19 23:18:58 +0100

    Delete "as" property in Link component

--- components/MenuItems/Works.js
+++ components/MenuItems/Works.js
@@ -5,9 +5,7 @@ export default function Works() {
   return (
     <div className={styles.works}>
       <li>
-        <Link href='/works/paintings' as='/paintings'>
-          Paintings
-        </Link>
+        <Link href='/works/paintings'>Paintings</Link>
       </li>
       <li>Graphic Art</li>
       <li>Posters</li>
