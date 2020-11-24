4aa32d87df42682497f0b80eadeaae432c08b682
Jakub Ziemba <jayziembka@gmail.com>
2020-11-24 18:26:03 +0100

    Change router to home

--- components/Header.js
+++ components/Header.js
@@ -5,12 +5,14 @@ import styles from './Header.module.scss';
 
 export default function Header({ title }) {
   const router = useRouter();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    router.push('/');
+  };
   return (
     <div className={styles.container}>
-      <FontAwesomeIcon
-        icon={faArrowLeft}
-        onClick={() => router.back()}
-      />
+      <FontAwesomeIcon icon={faArrowLeft} onClick={handleClick} />
       <div className={styles.header}>
         <h1>{title}</h1>
       </div>
