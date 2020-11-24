210dbe62eed1f5445272c1305d7283942ef9f5c3
Jakub Ziemba <jayziembka@gmail.com>
2020-11-24 01:37:14 +0100

    Delete layout=responsive

--- components/HomeImage.js
+++ components/HomeImage.js
@@ -9,7 +9,6 @@ export default function HomeImage() {
         quality={100}
         width={668}
         height={1000}
-        layout='responsive'
         loading='eager'
         alt='Photo of Roksana Machowska'
       />
