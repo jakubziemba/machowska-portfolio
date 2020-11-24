acecf00e6803b02cb49b313b5d60372db4400928
Jakub Ziemba <jayziembka@gmail.com>
2020-11-24 18:32:31 +0100

    Add layout components

--- pages/works/paintings.js
+++ pages/works/paintings.js
@@ -1,11 +1,15 @@
 import Header from '../../components/Header';
+import WorksHeader from '../../components/WorksHeader';
 import Work from './work';
+import Footer from '../../components/Footer';
 
 export default function Paintings() {
   return (
     <>
       <Header title='Paintings' />
-      <Work />
+      <WorksHeader />
+      <Work category='paintings' />
+      <Footer />
     </>
   );
 }
