d7c905b755f8cd73607fcc103d0ef295537e8f9b
Jakub Ziemba <jayziembka@gmail.com>
2020-11-25 22:29:23 +0100

    Change order of divs

--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,25 @@ import Image from 'next/image';
 import HomeHeader from '../components/HomeHeader';
 import Menu from '../components/Menu';
 import Footer from '../components/Footer';
-import styles from './image.module.scss';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.main}>
       <HomeHeader />
-      <div className={styles.main}>
-        <div className={styles.imageContainer}>
-          <Image
-            src='/images/me.png'
-            quality={100}
-            width={668}
-            height={1000}
-            layout='responsive'
-            priority={true}
-            alt='Photo of Roksana Machowska'
-          />
-        </div>
-        <Menu />
+      <div className={styles.imageContainer}>
+        <Image
+          src='/images/me.png'
+          quality={100}
+          width={668}
+          height={1000}
+          layout='responsive'
+          priority={true}
+          alt='Photo of Roksana Machowska'
+        />
       </div>
+      <Menu />
       <Footer />
-    </>
+    </div>
   );
 }
