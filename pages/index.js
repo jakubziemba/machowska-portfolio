c3c2aa551f502614622650eb79aa23879c9b77ec
Jakub Ziemba <jayziembka@gmail.com>
2020-11-26 01:02:54 +0100

    Add media query for larger screens

--- pages/index.js
+++ pages/index.js
@@ -7,19 +7,25 @@ import styles from './home.module.scss';
 export default function Home() {
   return (
     <div className={styles.main}>
-      <HomeHeader />
-      <div className={styles.imageContainer}>
-        <Image
-          src='/images/me.png'
-          quality={100}
-          width={668}
-          height={1000}
-          layout='responsive'
-          priority={true}
-          alt='Photo of Roksana Machowska'
-        />
+      <div className={styles.header}>
+        <HomeHeader />
       </div>
-      <Menu />
+      <div className={styles.main}>
+        <div className={styles.imageContainer}>
+          <Image
+            src='/images/me.png'
+            quality={100}
+            width={668}
+            height={1000}
+            layout='responsive'
+            priority={true}
+            alt='Photo of Roksana Machowska'
+          />
+        </div>
+        <div className={styles.menu}>
+          <Menu />
+        </div>
+      </div>
       <Footer />
     </div>
   );
