a4b7686c7efeb389d83e8d224041eb63d85092d2
Jakub Ziemba <jayziembka@gmail.com>
2020-11-26 21:36:19 +0100

    Delete unused code

--- components/Footer.js
+++ components/Footer.js
@@ -3,10 +3,6 @@ import styles from './Footer.module.scss';
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <a
-        href='https://instagram.com/roksana_machowska'
-        target='_blank'
-      ></a>
       <p className={styles.copy}>
         <span>&copy;</span> Roksana Machowska 2020: All rights
         reserved.
