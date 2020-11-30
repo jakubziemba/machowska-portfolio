4ec03c783d7d0194823e2dc53a5892be31c58c7a
Jakub Ziemba <jayziembka@gmail.com>
2020-11-30 13:17:08 +0100

    Create link to Works for accessibility

--- components/Menu.js
+++ components/Menu.js
@@ -1,5 +1,5 @@
-import Link from 'next/link';
 import { useState } from 'react';
+import Link from 'next/link';
 import Works from './MenuItems/Works';
 import styles from './Menu.module.scss';
 
@@ -16,7 +16,7 @@ export default function Menu() {
           className={worksActive ? styles.active : undefined}
           onClick={() => setWorksActive(!worksActive)}
         >
-          Works
+          <Link href='#'>Works</Link>
         </li>
         {worksActive && <Works />}
         <li>
