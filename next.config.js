5f06ba49470254d222d08400d9c0cba67c545f02
Jakub Ziemba <jayziembka@gmail.com>
2020-12-03 16:34:24 +0100

    Temporarily disable plugins

--- next.config.js
+++ next.config.js
@@ -1,19 +1,25 @@
-// next.config.js
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
-const imagemin = require('imagemin');
-const imageminMozjpeg = require('imagemin-mozjpeg');
+// // next.config.js
+// const withPlugins = require('next-compose-plugins');
+// const optimizedImages = require('next-optimized-images');
+// const imagemin = require('imagemin');
+// const imageminMozjpeg = require('imagemin-mozjpeg');
 
-module.exports = withPlugins([
-  [
-    optimizedImages,
-    {
-      /* config for next-optimized-images */
-      optimizeImagesInDev: true,
-      mozjpeg: { quality: 100 },
-    },
-  ],
-  [imagemin],
-  [imageminMozjpeg],
-  // your other plugins here
-]);
+// (async () => {
+//   await imagemin(['images/*/*/*.jpg'], 'public/images/*/*', {
+//     use: [imageminMozjpeg([{ quality: 100 }])],
+//   });
+
+//   console.log('Images optimized');
+// })();
+
+// module.exports = withPlugins([
+//   [
+//     optimizedImages,
+//     {
+//       /* config for next-optimized-images */
+//       optimizeImagesInDev: true,
+//       mozjpeg: { quality: 100 },
+//     },
+//   ],
+//   // your other plugins here
+// ]);
